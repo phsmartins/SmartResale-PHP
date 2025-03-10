7a2a3c493545760cba724236b8eebea5d45e2592
phsmartins <phsmartins2004@gmail.com>
2025-03-10 15:22:43 -0400

    refactoring to reduce the amount of if

--- src/Controller/User/LoginController.php
+++ src/Controller/User/LoginController.php
@@ -15,8 +15,7 @@ readonly class LoginController implements RequestHandlerInterface
 
     public function __construct(
         private UserRepository $userRepository,
-    )
-    {}
+    ) {}
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
@@ -25,36 +24,67 @@ readonly class LoginController implements RequestHandlerInterface
         $email = filter_var($parsedBody['email'], FILTER_VALIDATE_EMAIL);
         $password = filter_var($parsedBody['password']);
 
-        if ($email === false || $email === null) {
-            $this->addErrorMessage('Digite um e-mail válido');
-            return new Response(302, ['Location' => '/login']);
+        if (!$this->isValidEmail($email)) {
+            return $this->redirectWithError('/login', 'Digite um e-mail válido');
         }
 
         $_SESSION['user_email_login'] = $email;
 
         $userData = $this->userRepository->findUserByEmail($email);
 
-        if ($userData === null) {
-            $this->addErrorMessage('E-mail ou senha inválidos. Tente novamente');
-            return new Response(302, ['Location' => '/login']);
+        if (!$this->isValidUser($userData)) {
+            return $this->redirectWithError('/login', 'E-mail ou senha inválidos. Tente novamente');
         }
 
-        if ($userData->getIsActive() === 0) {
-            $this->addErrorMessage('Consta desativada. Ainda vou criar a funcionalidade para reativar &#128517;');
-            return new Response(302, ['Location' => '/login']);
+        if ($this->isInactiveUser($userData)) {
+            return $this->redirectWithError('/login', 'Conta desativada. Ainda vou criar a funcionalidade para reativar &#128517;');
         }
 
-        if (password_verify($password, $userData->getPassword() ?? '')) {
-            if (password_needs_rehash($userData->getPassword(), PASSWORD_ARGON2ID)) {
-                $this->userRepository->updatePassword($userData->getPassword(), $userData->getId());
-            }
-
-            $_SESSION['logged_in'] = true;
-            $_SESSION['user_id'] = $userData->getId();
-            return new Response(302, ['Location' => '/']);
+        if (!$this->isValidPassword($password, $userData)) {
+            return $this->redirectWithError('/login', 'E-mail ou senha inválidos. Tente novamente');
         }
 
-        $this->addErrorMessage('E-mail ou senha inválidos. Tente novamente');
-        return new Response(302, ['Location' => '/login']);
+        $this->loginUser($userData);
+        return new Response(302, ['Location' => '/']);
+    }
+
+    private function isValidEmail(?string $email): bool
+    {
+        return $email !== false && $email !== null;
+    }
+
+    private function isValidUser(?object $userData): bool
+    {
+        return $userData !== null;
+    }
+
+    private function isInactiveUser(object $userData): bool
+    {
+        return $userData->getIsActive() === 0;
+    }
+
+    private function isValidPassword(string $password, object $userData): bool
+    {
+        if (!password_verify($password, $userData->getPassword() ?? '')) {
+            return false;
+        }
+
+        if (password_needs_rehash($userData->getPassword(), PASSWORD_ARGON2ID)) {
+            $this->userRepository->updatePassword($userData->getPassword(), $userData->getId());
+        }
+
+        return true;
+    }
+
+    private function loginUser(object $userData): void
+    {
+        $_SESSION['logged_in'] = true;
+        $_SESSION['user_id'] = $userData->getId();
+    }
+
+    private function redirectWithError(string $location, string $message): ResponseInterface
+    {
+        $this->addErrorMessage($message);
+        return new Response(302, ['Location' => $location]);
     }
 }
